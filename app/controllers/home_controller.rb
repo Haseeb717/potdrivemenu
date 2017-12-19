64bee0649cf7adbee51e04fccadfdaf458169347
Haseeb717 <haseeb.ahmad717@gmail.com>
2017-12-19 21:27:31 +0500

    Add ordering

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@articles = Article.order('created_at').paginate(:page => params[:page], :per_page => 18)
+  	@articles = Article.order('created_at desc').paginate(:page => params[:page], :per_page => 18)
   	respond_to do |format|
 		  format.html # show.html.erb
 		  format.xml  { render :xml => @articles }
